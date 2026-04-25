1d7b662828c22705a8a87917a3ec6813fb33175d
GabaySimon <simon.gabay@student.ehb.be>
2026-04-25 23:01:52 +0200

    added flip card animation & delete functionality
    in collection view:
    - mosaic cards flip on hover to reveal back face
        --> back face shows delete button (later other buttons)
    - delete button removes mosaic from localStorage & re-renders collection

--- src/components/collections.js
+++ src/components/collections.js
@@ -1,4 +1,4 @@
-import { getMosaics } from "../services/storage"
+import { deleteMosaic, getMosaics } from "../services/storage"
 
 export const renderCollection = () => {
     const collectionGrid = document.getElementById('collection-grid');
@@ -11,18 +11,51 @@ export const renderCollection = () => {
         mosaicCard.classList.add('mosaic-card');
         collectionGrid.appendChild(mosaicCard);
 
-        const mosaicCardThumbnail = document.createElement('button');
+        const mosaicCardInner = document.createElement('div');
+        mosaicCardInner.classList.add('mosaic-card-inner');
+        mosaicCard.appendChild(mosaicCardInner);
+
+        const mosaicCardFront = document.createElement('div');
+        mosaicCardFront.classList.add('mosaic-card-front');
+        mosaicCardInner.appendChild(mosaicCardFront);
+
+        const mosaicCardBack = document.createElement('div');
+        mosaicCardBack.classList.add('mosaic-card-back');
+        mosaicCardInner.appendChild(mosaicCardBack);
+
+        const mosaicCardThumbnail = document.createElement('div');
         mosaicCardThumbnail.classList.add('mosaic-card-thumbnail');
         mosaicCardThumbnail.style.gridTemplateColumns = `repeat(${mosaic.gridSize}, 1fr)`;
-        mosaicCard.appendChild(mosaicCardThumbnail);
+        mosaicCardFront.appendChild(mosaicCardThumbnail);
 
         const mosaicCardName = document.createElement('span');
         mosaicCardName.classList.add('mosaic-card-name');
         mosaicCardName.textContent = mosaic.name;
         mosaicCard.appendChild(mosaicCardName);
 
+        const mosaicDeleteBtn = document.createElement('button');
+        mosaicDeleteBtn.classList.add('mosaic-delete-btn');
+        mosaicDeleteBtn.dataset.id = mosaic.id;
+        mosaicCardBack.appendChild(mosaicDeleteBtn);
+
+        mosaicDeleteBtn.addEventListener('click', (e) => {
+            deleteMosaic(mosaic.id);
+            renderCollection();
+        })
+
+        const deleteIcon = document.createElement('img');
+        deleteIcon.src = '/delete-icon.svg';
+        deleteIcon.alt = 'delete'
+        mosaicDeleteBtn.appendChild(deleteIcon);
+
         for (const track of mosaic.tracks) {
+            if (!track) {
+                const empty = document.createElement('div');
+                mosaicCardThumbnail.appendChild(empty);
+                continue;
+            }
             const cover = document.createElement('img');
+            cover.classList.add('thumbnail-cover');
             cover.dataset.track = track
             cover.src = track.album.cover_medium;
             cover.alt = 'Album cover';
